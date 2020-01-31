378aff97972fd07c18f7d279fcc0be072f3a7e4c
Dana Cronin <cronin43224235@gmail.com>
2020-01-31 15:18:05 -0500

    rearranging

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-// require("dotenv").config();
+require("dotenv").config();
 var express = require("express");
-// var mongojs = require("mongojs");
-// var axios = require("axios");
-var cheerio = require("cheerio");
 var app = express();
+var mongoose = require("mongoose");
 const path = require("path");
 const exphbs = require("express-handlebars");
 const router = require("./routes/index");
-
-// var databaseUrl = "";
-// var collections = [""];
+// var db = require("./models");
+// var databaseUrl = "mongnews";
+// var collections = ["articles", "comments"];
 // var db = mongojs(databaseUrl, collections);
 // db.on("error", function(error) {
 //   console.log("Database Error:", error);
@@ -21,25 +19,22 @@ app.set("views", path.join(__dirname, "/views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+mongoose.connect("mongodb://localhost/mongnews", { useNewUrlParser: true });
 
-
+// routers == ????
 app.use("/", router);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render("error");
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//     next(createError(404));
+// });
+// // error handler
+// app.use(function(err, req, res, next) {
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get("env") === "development" ? err : {};
+//     res.status(err.status || 500);
+//     res.render("error");
+// });
 
 app.listen(3000, function() {
 console.log("App running on port 3000!");
