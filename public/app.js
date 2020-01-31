e1db281b80a9a0e4f65cbf6f6a6c8c9cfe14df51
Dana Cronin <cronin43224235@gmail.com>
2020-01-31 15:18:33 -0500

    psudocoding needs

--- public/app.js
+++ public/app.js
@@ -1,9 +1,17 @@
 // Display Functionality
 
 // Event Listeners & on.load
-
-//Scrape new articles but don't display ones already saved!!!!?????
-
+// =======  1 ============
+// scrape 20
+// compare to saved
+// ///// & button for 20 new
+// ======== 2 ============
+// render saved
+// ====// 3 == unsave button?
+// ======== 4 ============
+// pull up comments from button
+// ====// 5 == delete button
+// ====// 6 == new comment form and submit button
 
 
 // function displayResults(animals) {
@@ -19,14 +27,17 @@
 //     $("tbody").append(tr);
 //   });
 // }
+
 // $.getJSON("/all", function(data) {
 //   displayResults(data);
 // });
+
 // $("#weight-sort").on("click", function() {
 //   $.getJSON("/weight", function(data) {
 //     displayResults(data);
 //   });
 // });
+
 // $("#name-sort").on("click", function() {
 //   $.getJSON("/name", function(data) {
 //     displayResults(data);
