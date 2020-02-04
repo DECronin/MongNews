6d3d5b98247d3e029f29f52131eb64d63b76d25c
Dana Cronin <cronin43224235@gmail.com>
2020-02-04 17:28:03 -0500

    re-assesing needs

--- public/app.js
+++ public/app.js
@@ -1,45 +1,9 @@
-// Display Functionality
+// Save Button
 
-// Event Listeners & on.load
-// =======  1 ============
-// scrape 20
-// compare to saved
-// ///// & button for 20 new
-// ======== 2 ============
-// render saved
-// ====// 3 == unsave button?
-// ======== 4 ============
-// pull up comments from button
-// ====// 5 == delete button
-// ====// 6 == new comment form and submit button
+// Unsave Button
 
+// Display Comments Button
 
-// function displayResults(animals) {
-//   $("tbody").empty();
-//   animals.forEach(function(animal) {
-//     var tr = $("<tr>").append(
-//       $("<td>").text(animal.name),
-//       $("<td>").text(animal.numLegs),
-//       $("<td>").text(animal.class),
-//       $("<td>").text(animal.weight),
-//       $("<td>").text(animal.whatIWouldReallyCallIt)
-//     );
-//     $("tbody").append(tr);
-//   });
-// }
+// Send New Comment Button
 
-// $.getJSON("/all", function(data) {
-//   displayResults(data);
-// });
-
-// $("#weight-sort").on("click", function() {
-//   $.getJSON("/weight", function(data) {
-//     displayResults(data);
-//   });
-// });
-
-// $("#name-sort").on("click", function() {
-//   $.getJSON("/name", function(data) {
-//     displayResults(data);
-//   });
-// });
+// Delete Comment Button
