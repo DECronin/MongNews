5c1bea255429fe96153403b62064d5df2219a93f
Dana Cronin <cronin43224235@gmail.com>
2020-02-04 15:47:25 -0500

    more psudo??

--- routes/saved.js
+++ routes/saved.js
@@ -16,3 +16,22 @@ const router = express.Router();
 // });
 
 module.exports = router;
+
+// app.post("/articles/:id", function (req, res) {
+//     db.Note.create(req.body)
+//       .then(function (dbNote) {
+//         // then find an article from the req.params.id
+//         console.log(dbNote.id);
+//         return db.Article.findByIdAndUpdate(req.params.id,
+  
+//           // and update it's "note" property with the _id of the new note
+//           { note: dbNote.id });
+//       })
+//       .then(function (dbArticle) {
+//         // If the User was updated successfully, send it back to the client
+//         res.json(dbArticle);
+//       })
+//       .catch(function (err) {
+//         res.json(err);
+//       });
+//   });
