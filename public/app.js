fde63ebf57bebdfdffaa093c4b57105765732f56
Dana Cronin <cronin43224235@gmail.com>
2020-02-04 22:45:08 -0500

    data-id now reads into console for further manipulation

--- public/app.js
+++ public/app.js
@@ -1,9 +1,49 @@
 // Save Button
+$("#save-btn").on("click", () => {
+    event.preventDefault();
+    let thisId = $(event.currentTarget).attr("data-num");
+    console.log(`save: ${thisId}`);
+    // $.ajax({
+    //     method: "POST",
+    //     url: "/articles/" + thisId + "save",
+    // }).then(location.reload())
+})
 
 // Unsave Button
+$("#unsave-btn").on("click", () => {
+    event.preventDefault();
+    let thisId = $(this).attr("data-id");
+    // console.log(`unsave: ${thisId}`);
+    // $.ajax({
+    //     method: "POST",
+    //     url: "/articles/" + thisId + "unsave",
+    // }).then(location.reload())
+})
 
 // Display Comments Button
+// $("#comments-btn")
+// set display to visible
+// grab article id for save
+// get method
 
 // Send New Comment Button
+// $("#submit-comment")
+// $.ajax({
+//         method: "POST",
+//         url: "/articles/" + thisId,
+//         data: {
+//           user: $("#userinput").val(),
+//           body: $("#bodyinput").val()
+//         }
+//       }).then(data => {
+//         //   append new divs
+//       })
 
 // Delete Comment Button
+// $("#delete-comment")
+// ajax post
+// grab id
+
+// Close Comments Display
+// $("#close-comments")
+// set css display to none
