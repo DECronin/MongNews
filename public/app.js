a9d4838e64be97efc0300ea623b19d5a61c53772
Dana Cronin <cronin43224235@gmail.com>
2020-02-05 17:47:40 -0500

    empty comment does not display and data-id's populate apropriately

--- public/app.js
+++ public/app.js
@@ -24,30 +24,25 @@ $(".comments-btn").on("click", () => {
     event.preventDefault();
     $(".comments-box").show();
     let thisId = $(event.currentTarget).attr("data-id");
-    console.log('click = ' + thisId);
     $(".comments-body").empty();
     $(".submit-comment").remove();
     $.ajax({
         method: "GET",
         url: "/api/comments/" + thisId
     }).then(data => {
-        console.log(`comments:\n========\n${JSON.stringify(data)}\n\nArray:\n`);
         let commentsArray = [data.comment];
-        console.log(commentsArray);
-        commentsArray.forEach(el => {
-            let newDiv = $("<div class='row comment-block'>");
-            newDiv.append(`<h3 class='li-user col-10'>${el.user}</h3>`);
-            newDiv.append(`<p class='li-body col-10'>${el.body}</p>`);
-            newDiv.append(`<button class='delete-comment col-2' data-id=${el.id}>X</button>`)
-            $(".comments-body").append(newDiv);
+        commentsArray.forEach((el, i) => {
+            if (i > 0){
+                let newDiv = $("<div class='row comment-block'>");
+                newDiv.append(`<h3 class='li-user col-10'>${el.user}</h3>`);
+                newDiv.append(`<p class='li-body col-10'>${el.body}</p>`);
+                newDiv.append(`<button class='delete-comment col-2' data-id=${el._id}>X</button>`)
+                $(".comments-body").append(newDiv);
+            }
         });
-        $(".comments-footer").append(`<button class='submit-comment' data-id=${data._id}>Send</button>`);
+        $(".comments-footer").append(`<button class='submit-comment' data-id='${data._id}'>Send</button>`);
     })
 })
-{/* <button id="submit-comment">Send</button> */}
-// set display to visible
-// grab article id for save
-// get method
 
 // Send New Comment Button
 // $(".submit-comment")
