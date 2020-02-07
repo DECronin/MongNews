03542624eb001b91ca03157741b5f58b0e135566
Dana Cronin <cronin43224235@gmail.com>
2020-02-06 21:41:55 -0500

    debugging connection?

--- server.js
+++ server.js
@@ -19,4 +19,8 @@ app.listen(3000, function() {
 console.log("App running on port 3000!");
 });
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI);
+
 module.exports = app;
